3a2060a8d2716bdfa600a252d12b3ea969f056f8
jarkad <jarkad@jarkad.net.eu.org>
2026-06-18 14:29:44 +0200

    get rid of gh cli

--- generate-app-token.sh
+++ generate-app-token.sh
@@ -4,6 +4,15 @@ encode_jwt_part() {
   openssl base64 | tr +/ -_ | tr -d '=\n'
 }
 
+api() {
+  local endpoint=$1
+  shift
+  curl -sL https://api.github.com/"$endpoint" "$@" \
+    -H 'Accept: application/vnd.github+json' \
+    -H "Authorization: Bearer $jwt" \
+    -H 'X-GitHub-Api-Version: 2026-03-10'
+}
+
 now=$(date +%s)
 issued_at=$(( now - 60 ))
 expire_at=$(( now + 600 ))
@@ -15,9 +24,9 @@ payload=$(jo iat="$issued_at" exp="$expire_at" iss="$CLIENT_ID" | encode_jwt_par
 signature=$(printf '%s.%s' "$header" "$payload" | openssl dgst -sha256 -sign <(printf '%s' "$PRIVATE_KEY") | encode_jwt_part)
 jwt=$(printf '%s.%s.%s' "$header" "$payload" "$signature")
 
-installation_id=$(gh api "$SCOPE"/installation -H "Authorization: Bearer $jwt" -q .id)
+installation_id=$(api "$SCOPE"/installation | jq -r .id)
 printf >&2 'Installation ID: %s\n' "$installation_id"
 
-token=$(gh api app/installations/"$installation_id"/access_tokens -X POST -H "Authorization: Bearer $jwt" -q .token)
+token=$(api app/installations/"$installation_id"/access_tokens -X POST | jq -r .token)
 
 printf '%s' "$token"
